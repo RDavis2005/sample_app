347a17ecc6199d0b2fa037e259c2962783255c4f
Rondale Davis <RDavis2005@msn.com>
2016-06-01 01:43:51 -0400

    Make a basic User model (including secure passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-    				 password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -12,23 +12,23 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-  	@user.name = "     "
-  	assert_not @user.valid?
+    @user.name = "     "
+    assert_not @user.valid?
   end
 
-  test "email should be presnt" do
-  	@user.email = "     "
-  	assert_not @user.valid?
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
   end
 
   test "name should not be too long" do
-  	@user.name = "a" * 51
-  	assert_not @user.valid?
+    @user.name = "a" * 51
+    assert_not @user.valid?
   end
 
   test "email should not be too long" do
-  	@user.email = "a" * 244 + "@example.com"
-  	assert_not @user.valid?
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
   end
 
   test "email validation should accept valid addresses" do
@@ -39,7 +39,7 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
-  		
+
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
