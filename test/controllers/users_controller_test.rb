1ce4cf4e0ed87a5065b6f251c07626f0240879a0
Rondale Davis <RDavis2005@msn.com>
2016-07-21 17:50:08 -0400

    Add account activations

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -36,7 +36,7 @@ class UsersControllerTest < ActionController::TestCase
   	assert_redirected_to root_url
   end
 
-  test "should redirect update when logged in s wrong user" do
+  test "should redirect update when logged in as wrong user" do
   	log_in_as(@other_user)
   	patch :update, id: @user, user: { name: @user.name, email: @user.email }
   	assert flash.empty?
